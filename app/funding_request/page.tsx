bc10fe3a96fd0d8c826386966ba1889c454bf9fd
kirellos-emad1 <k.wasfy02053@edu.dmu.edu.eg>
2024-03-11 21:24:06 +0200

    add responsive text

--- app/funding_request/page.tsx
+++ app/funding_request/page.tsx
@@ -6,7 +6,7 @@ const FundingRequest = () => {
       className="flex flex-col items-center justify-center w-full h-full py-10 bg-gray-300/20 "
     >
       <header>
-        <h1 className=" font-bold font-serif text-5xl text-blue-900/90">
+        <h1 className=" font-bold font-serif text-2xl m-2 lg:text-5xl md:text-3xl  text-blue-900/90">
           ٥٠ عام خبره في عالم السيارات
         </h1>
       </header>
