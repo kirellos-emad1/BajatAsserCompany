69087bf752ef9d1bedc1c1ede6df577e47305b01
kirellos-emad1 <k.wasfy02053@edu.dmu.edu.eg>
2024-03-18 03:49:38 +0200

    fix: car card in tablet view

--- components/home/CarCardWrapper.tsx
+++ components/home/CarCardWrapper.tsx
@@ -11,7 +11,7 @@ interface CarCardWrapperProps {
 
 export const CarCardWrapper = ({ children }: CarCardWrapperProps) => {
   return (
-    <Card className="lg:w-[350px] md:w-[250px] w-full shadow-md p-0">
+    <Card className="lg:w-[350px] md:w-[350px] w-full shadow-md p-0">
       <CardContent className="p-0">{children}</CardContent>
     </Card>
   );
