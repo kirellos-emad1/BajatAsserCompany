8717b33bd3ea04791978b027074714ed6a9fda87
kirellos-emad1 <k.wasfy02053@edu.dmu.edu.eg>
2024-03-19 05:20:45 +0200

    fix bug

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,12 +1,17 @@
-"use client"
 import React from "react";
 import { UserTableComponent } from "../_component/UserTableComponent";
 import { SessionProvider } from "next-auth/react";
+import { getAllUsers } from "@/data-access/user";
+import { stringify } from "querystring";
 
-const page = () => {
+
+const page = async ()  => {
+  const usersData = await getAllUsers()
+  
   return (
     <SessionProvider>
       <main className="h-screen">
+        <div>{JSON.stringify(usersData)}</div>
         <UserTableComponent />
       </main>
     </SessionProvider>
