3243682481ab972cd53b2cfccd6ac39cf09a996f
kirellos-emad1 <k.wasfy02053@edu.dmu.edu.eg>
2024-03-19 02:24:35 +0200

    bug

--- app/api/user-data/route.ts
+++ app/api/user-data/route.ts
@@ -1,6 +1,8 @@
 import { getAllUsers } from "@/data-access/user";
+import { NextResponse } from "next/server";
+
 export async function GET(request:Request) {
     const allUserData = await getAllUsers();
 
-    return Response.json(allUserData)
+    return  NextResponse.json(allUserData);
 }
