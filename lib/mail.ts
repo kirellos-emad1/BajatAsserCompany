76a4097a63c2b8649050a2e8bb214e8adb272b98
kirellos-emad1 <k.wasfy02053@edu.dmu.edu.eg>
2024-03-20 22:27:07 +0200

    fix mail bug

--- lib/mail.ts
+++ lib/mail.ts
@@ -4,7 +4,7 @@ const resend = new Resend(process.env.RESEND_API_KEY)
 
 
 export const sendPasswordResetEmail = async (email: string, token: string) => {
-    const resetLink = `${process.env.NEXT_PUBLIC_BASE_API_URL}/auth/new-password?token=${token}`;
+    const resetLink = `${process.env.NEXT_PUBLIC_BASE_API_URL || process.env.BASE_API_URL}/auth/new-password?token=${token}`;
     await resend.emails.send({
         from: "mail@bahjat-asser.com",
         to: email,
