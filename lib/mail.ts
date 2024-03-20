9921422de35afd065fc4186c923113260cda30ff
kirellos-emad1 <k.wasfy02053@edu.dmu.edu.eg>
2024-03-20 21:07:04 +0200

    make send valid for every email

--- lib/mail.ts
+++ lib/mail.ts
@@ -4,14 +4,14 @@ const resend = new Resend(process.env.RESEND_API_KEY)
 
 
 export const sendPasswordResetEmail = async (email: string, token: string) => {
-    const resetLink = `http://localhost:3000/auth/new-password?token=${token}`;
+    const resetLink = `${process.env.NEXT_PUBLIC_BASE_API_URL}/auth/new-password?token=${token}`;
     await resend.emails.send({
-        from: "onboarding@resend.dev",
+        from: "mail@bahjat-asser.com",
         to: email,
         subject: 'اعاده تعيين كلمة المرور',
         html: `
         <div>
-            <p> اضغط هنا <a href="${resetLink}">here</a> لاعادة تعيين كلمة المرور.</p>
+            <p> اضغط هنا <a href="${resetLink}"><strong>هنا</strong></a> لاعادة تعيين كلمة المرور.</p>
         </div>`
     })
 }
